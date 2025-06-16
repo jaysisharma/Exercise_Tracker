488b9377a10ddc742ba9cc166cd40dc95462794c
Jaysi Sharma <study.jaysi@gmail.com>
2025-06-16 15:08:43 +0545

    final commit

--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connection setup
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
