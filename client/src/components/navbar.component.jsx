c769553649aca3b47400039cf57a9046f940f4fb
Jaysi Sharma <study.jaysi@gmail.com>
2025-06-16 16:05:00 +0545

    final commit

--- client/src/components/navbar.component.jsx
+++ client/src/components/navbar.component.jsx
@@ -13,6 +13,9 @@ function Navbar() {
           <li className="navbar-item">
             <Link to="/create" className="nav-link">Create Exercise Log</Link>
           </li>
+           <li className="navbar-item">
+            <Link to="/create" className="nav-link">Create Exercise Log</Link>
+          </li>
           <li className="navbar-item">
             <Link to="/user" className="nav-link">Create User</Link>
           </li>
