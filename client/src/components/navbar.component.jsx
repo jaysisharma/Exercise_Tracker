fbe4c1e36742e5d82251202694e39c9b599c9935
Jaysi Sharma <study.jaysi@gmail.com>
2025-06-17 12:38:43 +0545

    docker compose modified

--- client/src/components/navbar.component.jsx
+++ client/src/components/navbar.component.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-      <Link to="/" className="navbar-brand">ExerciseTracker</Link>
+      <Link to="/" className="navbar-brand">Exercise Tracker</Link>
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
